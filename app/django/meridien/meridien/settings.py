e833ac2fbcf6b0a41e83c0ee3382ff0b710c3b1f
Chong Jing Quan <jingquan0302@gmail.com>
2020-05-22 17:21:49 +0800

    Update settings.py

--- app/django/meridien/meridien/settings.py
+++ app/django/meridien/meridien/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'rest_framework',
+    'rest_framework',
 	'bookings.apps.BookingConfig',
 	'emailtemplates.apps.EmailtemplatesConfig',
 	'items.apps.ItemsConfig',

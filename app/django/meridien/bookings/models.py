d22e60c9db07beac18c0f5f3acfd6ec0fe58fddc
JQChong <jingquan0302@gmail.com>
2020-05-23 21:57:40 +0800

    add deposit_left field

--- app/django/meridien/bookings/models.py
+++ app/django/meridien/bookings/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import *
 from items.models import BookedItem
 
 class Booking(models.Model):
@@ -7,6 +8,7 @@ class Booking(models.Model):
     loan_start_time = models.DateField(blank=False)
     loan_end_time = models.DateField(blank=False)
     booked_items = models.ManyToManyField(BookedItem)
+    deposit_left = models.DecimalField(default=0.00, decimal_places=2, max_digits=10, validators=[MinValueValidator(0.00), MaxValueValidator(200.00)])
     
 class Person(models.Model):
     name = models.CharField(max_length=256, blank=False)

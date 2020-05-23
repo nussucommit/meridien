ddda5460be51ab57ceccab42be02d4796ad1f297
NicholasCF <nicholas.fernando@u.nus.edu>
2020-05-23 13:34:16 +0800

    Fix style errors

--- app/django/meridien/bookings/models.py
+++ app/django/meridien/bookings/models.py
@@ -3,10 +3,10 @@ from items.models import BookedItem
 
 # Create your models here.
 class Booking(models.Model):
-    name = models.CharField(max_length=256, blank=False);
-    email = models.EmailField(blank=False);
-    reason = models.TextField(blank=False);
-    time_booked = models.DateTimeField(auto_now_add=True);
-    loan_start_time = models.DateField(blank=False);
-    loan_end_time = models.DateField(blank=False);
-    booked_items = models.ManyToManyField(BookedItem);
+    name = models.CharField(max_length=256, blank=False)
+    email = models.EmailField(blank=False)
+    reason = models.TextField(blank=False)
+    time_booked = models.DateTimeField(auto_now_add=True)
+    loan_start_time = models.DateField(blank=False)
+    loan_end_time = models.DateField(blank=False)
+    booked_items = models.ManyToManyField(BookedItem)

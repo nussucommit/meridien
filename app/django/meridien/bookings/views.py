78613ef208469c5ca64656cf5874cdb76e21f25c
NicholasCF <nicholas.fernando@u.nus.edu>
2020-05-24 13:08:30 +0800

    Add controller for giving booking list

--- app/django/meridien/bookings/views.py
+++ app/django/meridien/bookings/views.py
@@ -1,3 +1,12 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
+from bookings.models import Booking
+from bookings.serializers import BookingSerializer
+
 # Create your views here.
+def booking_list(request):
+    if request.method == 'GET':
+        bookings = Booking.objects.all()
+        bookings_serializer = BookingSerializer(bookings, many=True)
+        return JsonResponse({'booking_list': bookings_serializer.data})

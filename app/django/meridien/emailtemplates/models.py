de116bb0c2dab17ca0d6314af26bdc6c30b5db64
NicholasCF <nicholas.fernando@u.nus.edu>
2020-05-24 13:09:59 +0800

    Fix style errors

--- app/django/meridien/emailtemplates/models.py
+++ app/django/meridien/emailtemplates/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class EmailTemplate(models.Model):
-    name = models.CharField(max_length=256, blank=False);
-    template = models.TextField(blank=False);
+    name = models.CharField(max_length=256, blank=False)
+    template = models.TextField(blank=False)

9cdfeb9a4cfeb9a5bd36abf1a5f654df43449911
NicholasCF <nicholas.fernando@u.nus.edu>
2020-05-26 17:30:46 +0800

    Fix imports

--- app/django/meridien/emailtemplates/views.py
+++ app/django/meridien/emailtemplates/views.py
@@ -1,5 +1,7 @@
-from emailtemplates.models import *
 from django.views.decorators.csrf import csrf_exempt
+
+from emailtemplates.models import EmailTemplate
+from emailtemplates.serializers import EmailtemplateSerializer
 
 import sys
 sys.path.append('../')
@@ -7,8 +9,8 @@ from meridien import views_template
 
 @csrf_exempt
 def email_template_list(request):
-    return obj_list(request, EmailTemplate, EmailtemplateSerializer)
+    return views_template.obj_list(request, EmailTemplate, EmailtemplateSerializer)
 
 @csrf_exempt
 def email_template_detail(request, pk):
-    return obj_detail(request, pk, EmailTemplate, EmailtemplateSerializer)
+    return views_template.obj_detail(request, pk, EmailTemplate, EmailtemplateSerializer)
--- app/django/meridien/items/views.py
+++ app/django/meridien/items/views.py
@@ -8,16 +8,16 @@ from meridien import views_template
 
 @csrf_exempt
 def item_list(request):
-    return obj_list(request, Item, ItemSerializer)
+    return views_template.obj_list(request, Item, ItemSerializer)
 
 @csrf_exempt
 def item_detail(request, pk):
-    return obj_detail(request, pk, Item, ItemSerializer)
+    return views_template.obj_detail(request, pk, Item, ItemSerializer)
 
 @csrf_exempt
 def booked_item_list(request):
-    return obj_list(request, BookedItem, BookedItemSerializer)
+    return views_template.obj_list(request, BookedItem, BookedItemSerializer)
     
 @csrf_exempt
 def booked_item_detail(request, pk):
-    return obj_detail(request, pk, BookedItem, BookedItemSerializer)
+    return views_template.obj_detail(request, pk, BookedItem, BookedItemSerializer)

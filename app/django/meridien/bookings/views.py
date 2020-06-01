9817c492980b07650e93a5be1ec224b87bb1b7e8
NicholasCF <nicholas.fernando@u.nus.edu>
2020-06-01 18:44:04 +0800

    Add permission check to views

--- app/django/meridien/bookings/views.py
+++ app/django/meridien/bookings/views.py
@@ -2,14 +2,21 @@ from bookings.models import Booking
 from bookings.serializers import BookingSerializer
 from django.views.decorators.csrf import csrf_exempt
 
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+
 import sys
 sys.path.append('../')
 from meridien import views_template
 
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
 @csrf_exempt
 def booking_list(request):
     return views_template.obj_list(request, Booking, BookingSerializer)
 
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
 @csrf_exempt
 def booking_detail(request, pk):
     return views_template.obj_detail(request, pk, Booking, BookingSerializer)
--- app/django/meridien/emailtemplates/views.py
+++ app/django/meridien/emailtemplates/views.py
@@ -3,14 +3,21 @@ from django.views.decorators.csrf import csrf_exempt
 from emailtemplates.models import EmailTemplate
 from emailtemplates.serializers import EmailtemplateSerializer
 
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+
 import sys
 sys.path.append('../')
 from meridien import views_template
 
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
 @csrf_exempt
 def email_template_list(request):
     return views_template.obj_list(request, EmailTemplate, EmailtemplateSerializer)
 
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
 @csrf_exempt
 def email_template_detail(request, pk):
     return views_template.obj_detail(request, pk, EmailTemplate, EmailtemplateSerializer)

690a07bbd4edbe85e2b100dbf500b5f21c13f63a
JQChong <jingquan0302@gmail.com>
2020-06-02 21:30:37 +0800

    added pagination and filtering
    only filter by name, booking date and status (can add more on request)

--- app/angular/AngularView/src/app/app.module.ts
+++ app/angular/AngularView/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
@@ -13,6 +14,10 @@ import { MatInputModule } from '@angular/material/input';
 import { MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
+import { MatMomentDateModule } from "@angular/material-moment-adapter";
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 import { CreateItemComponent } from './create-item/create-item.component';
 import { ItemDetailsComponent } from './item-details/item-details.component';
@@ -37,16 +42,32 @@ import { BookingListComponent, BookingListDialog } from './booking-list/booking-
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
     MatToolbarModule,
     MatButtonModule,
     MatFormFieldModule,
     MatInputModule,
     MatTableModule,
     MatIconModule,
-    MatDialogModule
+    MatDialogModule,
+    MatDatepickerModule,
+    MatMomentDateModule,
+    MatPaginatorModule
   ],
-  exports: [ MatToolbarModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatTableModule, MatIconModule, MatDialogModule ],
-  providers: [],
+  exports: [ 
+    MatToolbarModule, 
+    MatButtonModule, 
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatTableModule, 
+    MatIconModule, 
+    MatDialogModule, 
+    MatDatepickerModule,
+    MatMomentDateModule,
+    MatPaginatorModule
+  ],
+  providers: [{provide: MAT_DATE_LOCALE, useValue: 'en-SG'},],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- app/angular/AngularView/src/app/booking-list/booking-list.component.ts
+++ app/angular/AngularView/src/app/booking-list/booking-list.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { Subject, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Component, OnInit, Inject, ViewChild } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AbstractControl, FormControl, Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 import { BookingsService } from '../model-service/bookings/bookings.service';
 import { Booking } from '../model-service/bookings/bookings';
@@ -8,21 +8,43 @@ import { BookedItem } from '../model-service/items/items';
 
 import { MatTableDataSource } from '@angular/material/table';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_MOMENT_DATE_FORMATS, MomentDateAdapter } from '@angular/material-moment-adapter';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+import { MatPaginator } from '@angular/material/paginator';
+
+import moment from 'moment';
 
 @Component({
   selector: 'booking-list',
   templateUrl: './booking-list.component.html',
-  styleUrls: ['./booking-list.component.scss']
+  styleUrls: ['./booking-list.component.scss'],
+  providers: [
+    {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
+    {provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS},
+  ],
 })
+
 export class BookingListComponent implements OnInit {
   
   bookings = new MatTableDataSource<Booking>();
-  tableColumns: string[] = ['id', 'name', 'email', 'organization', 'loan_start_time', 'loan_end_time', 'deposit_left', 'status'];
+  tableColumns: string[] = ['id', 'name', 'email', 'organization', 'time_booked', 'loan_start_time', 'loan_end_time', 'deposit_left', 'status'];
+  
+  filterForm: FormGroup;
+  
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
 
-  constructor(private bookingsService: BookingsService, public dialog: MatDialog) { }
+  constructor(private bookingsService: BookingsService, public dialog: MatDialog, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.reloadData();
+    this.bookings.paginator = this.paginator;
+    
+    this.filterForm = this.formBuilder.group({
+      name: ['',''],
+      fromDate: ['', this.dateCheck],
+      toDate: ['', this.dateCheck],
+      status: ['','']
+    });
   }
   
   reloadData() {
@@ -48,6 +70,46 @@ export class BookingListComponent implements OnInit {
           this.dialog.open(BookingListDialog, {width: '600px', data: final_data});
         }
       );
+  }
+  
+  filterByName(filterValue: string) {
+    this.bookings.filterPredicate = 
+      (data: Booking, filter: string) => data.name.indexOf(filter) != -1;
+    this.bookings.filter = filterValue.trim().toLowerCase();
+  }
+  
+  dateCheck(control: AbstractControl): any {
+    var d = control.value;
+    if(d!==null && d.length>0){
+      return moment(d);
+    }
+    return null;
+  }
+  
+  onSubmit(){
+    this.bookings.filterPredicate = this.bookingFilterPredicate;
+    this.bookings.filter=this.filterForm.value;
+  }
+  
+  bookingFilterPredicate(data: Booking, filter: any): boolean {
+    for(let value in filter){
+      if (filter[value]!==''&&filter[value]!==0) {
+        if (value === 'fromDate') {
+          if(Date.parse(data.time_booked)<Date.parse(filter[value])){
+            return false;
+          }
+        } else if (value === 'toDate') {
+          if(Date.parse(data.time_booked)>Date.parse(filter[value])+86399999){ //86399999 milliseconds is added to include the end point date
+            return false;
+          }
+        } else {
+          if(!data[value].includes(filter[value])){
+            return false;
+          }
+        }
+      }
+    }
+    return true;
   }
 
 }

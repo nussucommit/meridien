f98a2aa56ce46a99c271bf7c15839ccf285a06ff
NicholasCF <nicholas.fernando@u.nus.edu>
2020-06-06 12:37:23 +0800

    Add authorisation guard to pages

--- app/angular/AngularView/src/app/app-routing.module.ts
+++ app/angular/AngularView/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { AuthGuard } from './model-service/users/auth.guard';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -6,10 +7,10 @@ import { BookingListComponent } from './booking-list/booking-list.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
-    { path: 'items', component: ItemListComponent },
-    { path: 'bookings', component: BookingListComponent },
+    { path: 'items', component: ItemListComponent, canActivate: [AuthGuard] },
+    { path: 'bookings', component: BookingListComponent, canActivate: [AuthGuard] },
+    { path: '', redirectTo: 'bookings', pathMatch: 'full', canActivate: [AuthGuard] },
     { path: 'login', component: LoginFormComponent },
-    { path: '', redirectTo: 'bookings', pathMatch: 'full' },
     { path: '**', component: NotFoundComponent },
 ];
 

46de715e8011f2049b420258a3dfc8f043c0b236
JQChong <jingquan0302@gmail.com>
2020-06-09 01:50:23 +0800

    [wip] approval feature

--- app/django/meridien/bookings/urls.py
+++ app/django/meridien/bookings/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from bookings import views
 
 urlpatterns = [
-    url(r'^bookings/$', views.booking_list)
+    url(r'^bookings/$', views.booking_list),
+    url(r'^bookings/(?P<pk>[0-9]+)$', views.booking_detail)
 ]

5b3ffb2652f43f8c8f1aa8dd9836146155f9a384
NicholasCF <nicholas.fernando@u.nus.edu>
2020-06-08 22:01:45 +0800

    Add interceptors for authentication

--- app/angular/AngularView/src/app/app.module.ts
+++ app/angular/AngularView/src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { RefreshInterceptor } from './api-auth/refresh.interceptor';
+import { JwtInterceptor } from './api-auth/jwt.interceptor';
 import { LoginService } from './model-service/users/login.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -5,7 +7,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
@@ -53,7 +55,10 @@ import { LogoutComponent } from './logout/logout.component';
     ReactiveFormsModule
   ],
   exports: [ MatToolbarModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatTableModule, MatIconModule, MatDialogModule ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: RefreshInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

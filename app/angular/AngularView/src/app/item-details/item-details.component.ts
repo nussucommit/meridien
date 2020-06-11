65a50bcde5426af26e9c97f2131d67c4efa1285a
JQChong <jingquan0302@gmail.com>
2020-06-11 23:14:08 +0800

    add input validation for date and item form

--- app/angular/AngularView/src/app/item-details/item-details.component.ts
+++ app/angular/AngularView/src/app/item-details/item-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ItemsService } from '../model-service/items/items.service';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'item-details',
@@ -15,39 +15,43 @@ export class ItemDetailsComponent implements OnInit {
   itemForm: FormGroup;
 
   constructor(
-    public dialogRef: MatDialogRef<ItemDetailsComponent>, 
-    @Inject(MAT_DIALOG_DATA) public item_data: any, 
+    public dialogRef: MatDialogRef<ItemDetailsComponent>,
+    @Inject(MAT_DIALOG_DATA) public item_data: any,
     private formBuilder: FormBuilder,
     private itemsService: ItemsService
-    ) { }
+  ) { }
 
   ngOnInit(): void {
     this.item = this.item_data['item'];
 
     this.itemForm = this.formBuilder.group({
-      name: [this.item?this.item['name']:'',''],
-      category: [this.item?this.item['category']:'',''],
-      quantity: [this.item?this.item['quantity']:'',''],
-      deposit: [this.item?this.item['deposit']:'',''],
-      item_status: [this.item?this.item['item_status']:'',''],
-      remarks: [this.item?this.item['remarks']:'','']
+      name: [this.item ? this.item['name'] : '', Validators.required],
+      category: [this.item ? this.item['category'] : '', Validators.required],
+      quantity: [this.item ? this.item['quantity'] : '', Validators.required],
+      deposit: [this.item ? this.item['deposit'] : '', Validators.required],
+      item_status: [this.item ? this.item['item_status'] : '', Validators.required],
+      remarks: [this.item ? this.item['remarks'] : '', '']
     });
   }
 
-  getDialogTitle(){
-    if(this.item_data.mode==="create"){
+  getDialogTitle() {
+    if (this.item_data.mode === "create") {
       return "Create Item";
-    }else if(this.item_data.mode==="edit"){
+    } else if (this.item_data.mode === "edit") {
       return "Edit Item";
     }
   }
 
-  onSubmit(){
+  onSubmit() {
     this.dialogRef.close();
-    if(this.item_data.mode==="create"){
-      this.itemsService.createItem(this.itemForm.value).subscribe();
-    }else if(this.item_data.mode==="edit"){
-      this.itemsService.updateItem(this.item_data['item']['id'], this.itemForm.value).subscribe();
+    var data = this.itemForm.value;
+    if (data.remarks.length === 0) {
+      data.remarks = 'N/A';
+    }
+    if (this.item_data.mode === "create") {
+      this.itemsService.createItem(data).subscribe();
+    } else if (this.item_data.mode === "edit") {
+      this.itemsService.updateItem(this.item_data['item']['id'], data).subscribe();
     }
   }
 }

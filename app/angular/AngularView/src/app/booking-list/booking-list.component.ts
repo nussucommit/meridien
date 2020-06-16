36417ea3158940d56d5df7bc845515265f91ee32
JQChong <jingquan0302@gmail.com>
2020-06-16 19:36:29 +0800

    change date checking code and code refactoring

--- app/angular/AngularView/src/app/booking-list/booking-list.component.ts
+++ app/angular/AngularView/src/app/booking-list/booking-list.component.ts
@@ -22,8 +22,8 @@ import moment from 'moment';
   templateUrl: './booking-list.component.html',
   styleUrls: ['./booking-list.component.scss'],
   providers: [
-    {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
-    {provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS},
+    { provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE] },
+    { provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS },
   ],
 })
 
@@ -31,29 +31,29 @@ export class BookingListComponent implements OnInit {
 
   bookings = new MatTableDataSource<Booking>();
   tableColumns: string[] = ['id', 'name', 'email', 'organization', 'time_booked', 'loan_start_time', 'loan_end_time', 'deposit_left', 'status'];
-  
+
   filterForm: FormGroup;
 
   bookingDialogOpened = false;
-  
-  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
-  @ViewChild(MatSort, {static: true}) sort: MatSort;
+
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
 
   constructor(private bookingsService: BookingsService, public dialog: MatDialog, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.reloadData();
     this.bookings.paginator = this.paginator;
-    this.bookings.sort=this.sort;
-    
+    this.bookings.sort = this.sort;
+
     this.filterForm = this.formBuilder.group({
-      name: ['',''],
+      name: ['', ''],
       fromDate: ['', this.dateCheck],
       toDate: ['', this.dateCheck],
-      status: ['','']
+      status: ['', '']
     });
   }
-  
+
   reloadData() {
     this.bookingsService.getBookingList()
       .subscribe(
@@ -62,15 +62,15 @@ export class BookingListComponent implements OnInit {
         }
       );
   }
-  
+
   openDialog(row: { [x: string]: number; }) {
     this.bookingsService.getBookedItemsByBooker(row['id'])
       .subscribe(
         (data: BookedItem[]) => {
-          if(!this.bookingDialogOpened){
+          if (!this.bookingDialogOpened) {
             this.bookingDialogOpened = true;
-            let dialogRef = this.dialog.open(BookingListDialog, {width: '600px', data: {source: row, booked_items: data}});
-            dialogRef.afterClosed().subscribe(()=>{
+            let dialogRef = this.dialog.open(BookingListDialog, { width: '600px', data: { source: row, booked_items: data } });
+            dialogRef.afterClosed().subscribe(() => {
               this.reloadData();
               this.bookingDialogOpened = false;
             });
@@ -78,33 +78,31 @@ export class BookingListComponent implements OnInit {
         }
       );
   }
-  
+
   dateCheck(control: AbstractControl): any {
     var d = control.value;
-    if(d!==null && d.length>0){
-      return moment(d);
-    }
-    return null;
+    return (d === null || (typeof d === 'string') && d.length === 0 || moment(d).isValid()) ? null : { date: true };
   }
-  
-  onSubmit(){
+
+  onSubmit() {
     this.bookings.filterPredicate = this.bookingFilterPredicate;
-    this.bookings.filter=this.filterForm.value;
+    this.bookings.filter = this.filterForm.value;
   }
-  
+
   bookingFilterPredicate(data: Booking, filter: any): boolean {
-    for(let value in filter){
-      if (filter[value]!==''&&filter[value]!==0) {
+    for (let value in filter) {
+      if (filter[value] !== '' && filter[value] !== 0) {
         if (value === 'fromDate') {
-          if(Date.parse(data.time_booked.toString())<Date.parse(filter[value])){
+          if (Date.parse(data.time_booked.toString()) < Date.parse(filter[value])) {
             return false;
           }
         } else if (value === 'toDate') {
-          if(Date.parse(data.time_booked.toString())>Date.parse(filter[value])+86399999){ //86399999 milliseconds is added to include the end point date
+          //86399999 milliseconds is added to include the end point date
+          if (Date.parse(data.time_booked.toString()) > Date.parse(filter[value]) + 86399999) {
             return false;
           }
         } else {
-          if(!data[value].includes(filter[value])){
+          if (!data[value].includes(filter[value])) {
             return false;
           }
         }
@@ -120,22 +118,22 @@ export class BookingListComponent implements OnInit {
 })
 export class BookingListDialog {
   tableColumnsBookedItems: string[] = ['name', 'quantity', 'status'];
-  constructor(private bookingsService: BookingsService, public dialogRef: MatDialogRef<BookingListDialog>, @Inject(MAT_DIALOG_DATA) public booking_data: any, private _snackbar: MatSnackBar) {}
+  constructor(private bookingsService: BookingsService, public dialogRef: MatDialogRef<BookingListDialog>, @Inject(MAT_DIALOG_DATA) public booking_data: any, private _snackbar: MatSnackBar) { }
 
-  updateStatus(status: string){
-    var booking_data_copy = {...this.booking_data.source};
+  updateStatus(status: string) {
+    var booking_data_copy = { ...this.booking_data.source };
     delete booking_data_copy['id'];
     booking_data_copy['status'] = status;
     this.bookingsService.updateBooking(this.booking_data.source.id, booking_data_copy).subscribe();
     this.dialogRef.close();
 
     var snackbarString = '';
-    if(status==='PEN'){
+    if (status === 'PEN') {
       snackbarString = 'Pending';
-    }else if(status==='PRO'){
+    } else if (status === 'PRO') {
       snackbarString = 'Processed';
     }
 
-    this._snackbar.open('Status of Booking #'+this.booking_data.source.id+' changed to: '+snackbarString, 'OK', {duration: 5000,});
+    this._snackbar.open('Status of Booking #' + this.booking_data.source.id + ' changed to: ' + snackbarString, 'OK', { duration: 5000, });
   }
 }

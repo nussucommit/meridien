aac53a131eeb0d5d2112b265ca3433f4a0770c8e
JQChong <jingquan0302@gmail.com>
2020-06-17 02:02:24 +0800

    change behaviour of snackbar

--- app/angular/AngularView/src/app/booking-details/booking-details.component.ts
+++ app/angular/AngularView/src/app/booking-details/booking-details.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, AbstractControl, FormArray } from '@angular/forms';
 import { BookingsService } from '../model-service/bookings/bookings.service';
 
 import { MAT_MOMENT_DATE_FORMATS, MomentDateAdapter } from '@angular/material-moment-adapter';
 import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
 import moment from 'moment';
+import { Items } from '../model-service/items/items';
+import { ItemsService } from '../model-service/items/items.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'booking-details',
@@ -20,7 +23,13 @@ export class BookingDetailsComponent implements OnInit {
   detailsForm: FormGroup;
   itemsForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private bookingsService: BookingsService) { }
+  itemArray: Items[];
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private bookingsService: BookingsService,
+    private itemsService: ItemsService
+  ) { }
 
   ngOnInit() {
     this.detailsForm = this.formBuilder.group({
@@ -31,6 +40,14 @@ export class BookingDetailsComponent implements OnInit {
       loan_start_time: ['', [Validators.required, this.dateCheck]],
       loan_end_time: ['', [Validators.required, this.dateCheck]],
     });
+
+    this.itemsForm = this.formBuilder.group({ items: this.formBuilder.array([this.newItemInput()]) });
+
+    this.itemsService.getItemsList().subscribe(
+      data => {
+        this.itemArray = data;
+      }
+    );
   }
 
   dateCheck(control: AbstractControl): any {
@@ -40,6 +57,25 @@ export class BookingDetailsComponent implements OnInit {
   emailCheck(control: AbstractControl): any {
     let regex = new RegExp('e[0-9]{7}@u\.nus\.edu');
     return regex.test(control.value) ? null : { email: true };
+  }
+
+  newItemInput(): FormGroup {
+    return this.formBuilder.group({
+      item: ['', Validators.required],
+      quantity: [1, [Validators.required, Validators.min(1)]]
+    });
+  }
+
+  getItemInputForm(): FormArray {
+    return this.itemsForm.get('items') as FormArray;
+  }
+
+  addBlankItem() {
+    this.getItemInputForm().push(this.newItemInput());
+  }
+
+  removeItemInput(i: number) {
+    this.getItemInputForm().removeAt(i);
   }
 
   step1() {

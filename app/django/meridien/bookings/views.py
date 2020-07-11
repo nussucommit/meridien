06198d78e67f90505ebd31ac79d699911a5d4209
NicholasCF <nicholas.fernando@u.nus.edu>
2020-07-11 14:51:46 +0800

    Add API to confirm or delete booking

--- app/django/meridien/bookings/views.py
+++ app/django/meridien/bookings/views.py
@@ -1,13 +1,14 @@
-import json
-
+from django.http.response import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
+from rest_framework.parsers import JSONParser
 from rest_framework.permissions import IsAuthenticated
 
 from bookings.models import Booking
 from bookings.serializers import BookingSerializer
 from confirmationemails.templates import send_confirmation_email
+from confirmationemails.tokens import decode_token
 
 import sys
 sys.path.append('../')
@@ -21,7 +22,7 @@ def booking_list(request):
     response = views_template.obj_list(request, Booking, BookingSerializer)
     if request.method == 'POST':
         if response.status_code == status.HTTP_200_OK:
-            send_confirmation_email(json.loads(response.content))
+            send_confirmation_email(JSONParser().parse(response.content))
             return response
     else:
         return response
@@ -32,3 +33,26 @@ def booking_list(request):
 @csrf_exempt
 def booking_detail(request, pk):
     return views_template.obj_detail(request, pk, Booking, BookingSerializer)
+
+
+@api_view(['GET', 'DELETE'])
+@csrf_exempt
+def booking_detail_token(request, token):
+    try:
+        booking_id = decode_token(token)
+    except Exception:
+        return JsonResponse({"message": "Invalid token"}, status=status.HTTP_403_FORBIDDEN)
+    return views_template.obj_detail(request, booking_id, Booking, BookingSerializer)
+
+
+@api_view(['PATCH'])
+@csrf_exempt
+def confirm_booking_token(request, token):
+    try:
+        booking_id = decode_token(token)
+    except Exception:
+        return JsonResponse({"message": "Token not valid"}, status=status.HTTP_403_FORBIDDEN)
+    booking = Booking.objects.get(pk=booking_id)
+    booking.status = Booking.PENDING
+    booking.save()
+    return JsonResponse({"message": "Success"}, status=status.HTTP_200_OK)

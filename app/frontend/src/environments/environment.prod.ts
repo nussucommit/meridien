846b8bf606c8d75b3325666f88e7c357e947b5f2
NicholasCF <nicholas.fernando@u.nus.edu>
2020-12-19 17:19:16 +0800

    Update backend link in frontend settings

--- app/frontend/src/environments/environment.prod.ts
+++ app/frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://meridien-backend.herokuapp.com/api/',
+  apiUrl: 'https://meridien-backend-test.herokuapp.com/api/',
   apiKey: 'bchme1shvzqaiv20vldopiru78j82mtwz8bkj3464muxs0s5'
 };

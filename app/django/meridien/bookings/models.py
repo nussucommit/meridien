4c7e51cf0904b68ca068daeed3f65da86df16c45
WM71811 <55304760+WM71811@users.noreply.github.com>
2020-12-31 01:41:42 +0800

    Change to flag

--- app/django/meridien/bookings/models.py
+++ app/django/meridien/bookings/models.py
@@ -21,7 +21,7 @@ class Booking(models.Model):
     ]
 
     phone_number_validator = RegexValidator(r'(8[1-8][0-9]{6}|9[0-8][0-9]{6})', 'Invalid phone number.')
-    
+
     name = models.CharField(max_length=256, blank=False, default='N/A')
     email = models.EmailField(blank=False, default='N/A')
     organization = models.CharField(max_length=1000, blank=False, default='N/A')
@@ -31,7 +31,7 @@ class Booking(models.Model):
     loan_start_time = models.DateField(blank=False, default=date.fromtimestamp(0))
     loan_end_time = models.DateField(blank=False, default=date.fromtimestamp(0))
     deposit_left = models.DecimalField(default=0.00, decimal_places=2, max_digits=10, validators=[MinValueValidator(0.00), MaxValueValidator(200.00)])
-    amount_paid = models.DecimalField(default=0.00, decimal_places=2, max_digits=10, validators=[MinValueValidator(0.00), MaxValueValidator(200.00)])
+    deposit_paid = models.BooleanField(default=False)
     status = models.CharField(max_length=32, blank=False, choices=PROCESS_STATUSES, default=UNCONFIRMED)
 
     def __str__(self):

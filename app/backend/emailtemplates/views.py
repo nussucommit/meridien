13ad7d2e3fcae22af382c346336ecddeae01bd1b
NicholasCF <nicholas.fernando@u.nus.edu>
2021-02-27 23:18:53 +0800

    Hack to populate template

--- app/backend/emailtemplates/views.py
+++ app/backend/emailtemplates/views.py
@@ -1,10 +1,15 @@
+import json
 from django.views.decorators.csrf import csrf_exempt
+from django.http.response import JsonResponse
 
 from emailtemplates.models import EmailTemplate
 from emailtemplates.serializers import EmailtemplateSerializer
 
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+
+from emailtemplates.hack import populate_template
 
 import sys
 sys.path.append('../')
@@ -21,3 +26,15 @@ def email_template_list(request):
 @csrf_exempt
 def email_template_detail(request, pk):
     return views_template.obj_detail(request, pk, EmailTemplate, EmailtemplateSerializer)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+@csrf_exempt
+def email_template_detail_populated(request, pk, booking_pk):
+    response = views_template.obj_detail(request, pk, EmailTemplate, EmailtemplateSerializer)
+    if response.status_code != status.HTTP_200_OK:
+        return response
+    else:
+        data = json.loads(response.content.decode('utf-8'))
+        data["template"] = populate_template(data["template"], booking_pk)
+        return JsonResponse(data=data, status=status.HTTP_200_OK)
